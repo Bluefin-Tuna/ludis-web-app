460d2c7b8da8f60de6b7a99a6cb0a042ee13f106
mohsinz99 <59673807+mohsinz99@users.noreply.github.com>
2022-05-19 18:31:10 -0700

    Added email/password auth

--- ludis/app/routes/auth.py
+++ ludis/app/routes/auth.py
@@ -1,78 +1,111 @@
-from functools import wraps
-from flask import jsonify, Blueprint
-import flask
-import google_auth_oauthlib
+# from functools import wraps
+# from flask import jsonify, Blueprint
+# import flask
+# import google_auth_oauthlib
+#
+# auth = Blueprint("auth", __name__)
+#
+# def login_required(test):
+#
+#     @wraps(test)
+#     def wrap(*args, **kwargs):
+#
+#         return
+#
+#     return wrap
+#
+# def admin_required(test):
+#
+#     @wraps(test)
+#     def wrap(*args, **kwargs):
+#
+#         return
+#
+#     return
+#
+# @auth.route('/auth/sign-in')
+# def sign_in():
+#
+#     return
+#
+# @auth.route('/auth/authorize')
+# def authorize():
+#
+#     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
+#     flow.redirect_uri = flask.url_for('callback', _external=True)
+#
+#     authorization_url, state = flow.authorization_url(
+#         access_type='offline',
+#         include_granted_scopes='true')
+#
+#     flask.session['state'] = state
+#
+#     return flask.redirect(authorization_url)
+#
+#
+# @auth.route('/auth/callback')
+# def callback():
+#
+#     state = flask.session['state']
+#
+#     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+#         CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+#     flow.redirect_uri = flask.url_for('callback', _external=True)
+#
+#     authorization_response = flask.request.url
+#     flow.fetch_token(authorization_response=authorization_response)
+#
+#     credentials = flow.credentials
+#     flask.session['credentials'] = credentials_to_dict(credentials)
+#
+#     return flask.redirect(flask.url_for('sign_in'))
+#
+# @auth.route('/auth/sign-out')
+# @login_required
+# def sign_out():
+#
+#     return
+#
+# def credentials_to_dict(credentials):
+#
+#     return {
+#         'token': credentials.token,
+#         'refresh_token': credentials.refresh_token,
+#         'token_uri': credentials.token_uri,
+#         'client_id': credentials.client_id,
+#         'client_secret': credentials.client_secret,
+#         'scopes': credentials.scopes
+#     }
 
-auth = Blueprint("auth", __name__)
+import pyrebase
+# import firebase_admin
+# from firebase_admin import credentials
 
-def login_required(test):
+config = {
+  "apiKey": "AIzaSyDE0qn7eQH9NQy1AlHDXg1rsdFw6Z6RcKU",
+  "authDomain": "ludiswebapp.firebaseapp.com",
+  "projectId": "ludiswebapp",
+  "storageBucket": "ludiswebapp.appspot.com",
+  "messagingSenderId": "1060610658237",
+  "appId": "1:1060610658237:web:3fd927055703f383420cd4",
+  "measurementId": "G-02HN41C522"
+}
 
-    @wraps(test)    
-    def wrap(*args, **kwargs):
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
-        return
+# EMAIL AUTH
+def signup():
+  # redirect to signup page
+  try:
+    user = auth.create_user_with_email_and_password(email, password)
+  except:
+#     if duplicate email
 
-    return wrap
+def login():
+#  redirect to login page
+  login = auth.sign_in_with_email_and_password(email, password)
 
-def admin_required(test):
-
-    @wraps(test)
-    def wrap(*args, **kwargs):
-
-        return
-
-    return
-
-@auth.route('/auth/sign-in')
-def sign_in():
-
-    return
-
-@auth.route('/auth/authorize')
-def authorize():
-
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
-    flow.redirect_uri = flask.url_for('callback', _external=True)
-
-    authorization_url, state = flow.authorization_url(
-        access_type='offline',
-        include_granted_scopes='true')
-
-    flask.session['state'] = state
-
-    return flask.redirect(authorization_url)
-
-
-@auth.route('/auth/callback')
-def callback():
-    
-    state = flask.session['state']
-
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
-    flow.redirect_uri = flask.url_for('callback', _external=True)
-
-    authorization_response = flask.request.url
-    flow.fetch_token(authorization_response=authorization_response)
-
-    credentials = flow.credentials
-    flask.session['credentials'] = credentials_to_dict(credentials)
-
-    return flask.redirect(flask.url_for('sign_in'))
-
-@auth.route('/auth/sign-out')
-@login_required
-def sign_out():
-    
-    return
-
-def credentials_to_dict(credentials):
-
-    return {
-        'token': credentials.token,
-        'refresh_token': credentials.refresh_token,
-        'token_uri': credentials.token_uri,
-        'client_id': credentials.client_id,
-        'client_secret': credentials.client_secret,
-        'scopes': credentials.scopes
-    }
+# GOOGLE AUTH
+# cred = credentials.Certificate("path/to/serviceAccountKey.json")
+# firebase_admin.initialize_app(cred)
