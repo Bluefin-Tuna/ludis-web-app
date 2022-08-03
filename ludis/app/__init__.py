85f17679be751e2e58c0d9118de20b833abe86fc
DumbassNamedTuna <50528857+DumbassNamedTuna@users.noreply.github.com>
2022-08-03 01:48:03 -0700

    Initial migration

--- ludis/app/__init__.py
+++ ludis/app/__init__.py
@@ -1,3 +1,14 @@
+from flask import Flask
+from ludis.schemas.sql import db
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite3'
+
+migrate = Migrate()
+
 def create_app():
 
+    db.init_app(app)
+    migrate.init_app(app, db)
     return
